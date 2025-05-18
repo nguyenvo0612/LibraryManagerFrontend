fc06cdd2572987598f721283f66f88f352ad1c98
nguyenvo0612 <ngvo.1203@gmail.com>
2025-05-18 17:30:59 +0700

    first commit

---
+++ LibraryManagerBackEnd/src/main/java/com/librarymanager/backend/LibraryManagerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.librarymanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerBackEndApplication.class, args);
+    }
+
+}
---
+++ LibraryManagerBackEnd/src/test/java/com/librarymanager/backend/LibraryManagerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
